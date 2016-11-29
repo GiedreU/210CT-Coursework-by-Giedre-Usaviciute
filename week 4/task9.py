e991952fcd80dd7f64382e53beef1593542384be
GiedreU <giedreusaviciute@gmail.com>
2016-11-29 21:33:32 +0000

    Update task9.py

--- week 4/task9.py
+++ week 4/task9.py
@@ -1,12 +1,13 @@
 def binary_search(aList, low, high):
+    '''returns true if value within given interval is found, else returns false'''
     if len(aList)==0:
         return False
     mid = len(aList)//2
     if aList[mid]>= low and aList[mid]<=high:
         return True
-    elif aList[mid] < low and len(aList) != 0:
+    elif aList[mid] < low:
         return binary_search(aList[(mid+1):], low, high)
-    elif aList[mid] > high and len(aList) != 0:
+    elif aList[mid] > high:
         return binary_search(aList[:(mid-1)], low, high)
 
 aList = [2,3,5,7,9,9,9,11,13]
