77ea72c1c3acac10e71485189d5df0fdb438423e
GiedreU <giedreusaviciute@gmail.com>
2016-11-30 03:07:50 +0000

    Update graph2.py

--- week 7/graph2.py
+++ week 7/graph2.py
@@ -1,94 +1,81 @@
 from queue import *
 
+class E(object):
+    def __init__(self, label, edges = []):
+        self.label = label
+        self.edges = []
 
-class Node(object):
-    def __init__(self,node, linkedTo):
-        self.node = node
-        self.linkedTo = []
-
+class G(object):
+    def __init__(self):
+        self.graph = []
     
+    def add(self,e):
+        self.graph.append(e)
+        
+    def edge(self, e1, e2):
+        for i in self.graph:
+            if i == e1 and e2 not in i.edges:
+                i.edges.append(e2)
+            if i == e2 and e1 not in i.edges:
+                i.edges.append(e1)
 
-
-graph = []
-
-def insert_node(graph, node):
-    graph.append(Node(node,None))
-
-def insert_link(graph, link):
-    n1,n2 = link
-    if len(graph)!=0:
-        for item in graph:
-            if item.node == n1:
-                if n2 not in item.linkedTo:
-                    item.linkedTo.append(n2)
-            if item.node == n2:
-                if n1 not in item.linkedTo:
-                    item.linkedTo.append(n1)
-
-def depth(graph,v):
-    visited = []
+def depthFirstSearch(G, v):
     S = []
-    S.append(v)           
+    visited = []
+    S.append(v)
     while len(S)!=0:
         u = S.pop()
         if u not in visited:
             visited.append(u)
-            for i in graph:
-                if i.node == u:
-                    for j in i.linkedTo:
-                        S.append(j)
-
+            for e in u.edges:
+                S.append(e)
     return visited
 
-
-def breadth(graph, v):
-    visited = []
+def breadthFirstSearch(G,v):
     Q = Queue(maxsize = 0)
-    Q.put(v)             
+    visited = []
+    Q.put(v)
     while not Q.empty():
         u = Q.get()
         if u not in visited:
             visited.append(u)
-            for i in graph:
-                if i.node == u:
-                    for j in i.linkedTo:
-                        Q.put(j)
-
+            for e in u.edges:
+                Q.put(e)
     return visited
 
-                
-        
-             
-        
-        
+node1 = E(1,None)
+node2 = E(2,None)
+node3 = E(3,None)
+node4 = E(4,None)
+node5 = E(5,None)
+node6 = E(6,None)
+node7 = E(7,None)
+node8 = E(8,None)
+node9 = E(9,None)
+graph = G()
+graph.add(node1)
+graph.add(node2)
+graph.add(node3)
+graph.add(node4)
+graph.add(node5)
+graph.add(node6)
+graph.add(node7)
+graph.add(node8)
+graph.add(node9)
+graph.edge(node1,node2)
+graph.edge(node1,node3)
+graph.edge(node3,node4)
+graph.edge(node3,node5)
+graph.edge(node4,node6)
+graph.edge(node4,node7)
+graph.edge(node7,node5)
+graph.edge(node5,node9)
+graph.edge(node9,node8)
+graph.edge(node2,node1)
 
-insert_node(graph, 1)
-insert_node(graph, 2)
-insert_node(graph, 3)
-insert_node(graph, 4)
-insert_node(graph, 5)
-insert_node(graph, 6)
-insert_node(graph, 7)
-insert_node(graph, 8)
-insert_node(graph, 9)
-insert_link(graph,(1,2))
-insert_link(graph,(1,3))
-insert_link(graph,(3,4))
-insert_link(graph,(3,5))
-insert_link(graph,(4,6))
-insert_link(graph,(4,8))
-insert_link(graph,(4,7))
-insert_link(graph,(7,5))
-insert_link(graph,(5,9))
-insert_link(graph,(9,8))
-insert_link(graph,(2,1))
-
-
-
-dep = []
-dep = depth(graph, 1)
-b =[]
-b = breadth(graph,1)
-print(b)
-print (dep)
-
+dep =depthFirstSearch(graph, node1)
+bre = breadthFirstSearch(graph, node1)
+for i in dep:
+    print(i.label)
+for i in bre:
+    print(i.label)
